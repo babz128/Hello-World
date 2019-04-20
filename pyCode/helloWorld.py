ae1735642c848fa0ffe8c104490fa8926e9c229a
Babz <babz@Babzs-MacBook-Pro.local>
2019-04-19 23:25:34 -0400

    update: Codacy style complaints fix

--- pyCode/helloWorld.py
+++ pyCode/helloWorld.py
@@ -3,14 +3,13 @@
 # installed termcolor project using pip install: "python3 -m pip install termcolor"
 # Tried blessings project, but only compatible with python 2.7
 
-import sys
-from termcolor import colored, cprint
+from termcolor import colored
 
 def myPrint():
     print('Hello World')
 
 def myPrint2(entry):
-    print("Hi " + entry + ", Welcome to the Hello World repo") 
+    print("Hi" + entry + ", Welcome to the Hello World repo") 
 
 def myPrint3(entry):
     print("\033[1;31;40m Hi " + entry + ", Welcome to a colorful World")
@@ -20,7 +19,7 @@ def myPrint4(entry):
     print(colored("hello!!! RED with colorful", "red"))
     print(colored("hello " + entry + ", Nice to meet you", "blue"))
     #on_red for background, attrs for bold, underline, blink, reverse, concealed, dark etc
-    print(colored("Hey!!! RED with color", "green", 'on_red', attrs=['underline'])) 
+    print(colored("Hey!!!RED with color", "green", 'on_red', attrs=['underline'])) 
 
 # Function calls
 myPrint()
